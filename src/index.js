90f019f5607044c504d339b3d5d13eceef745bc2
Oksana Sergeeva <oksanasergeeva@Oksanas-MacBook-Pro.local>
2022-10-01 19:59:01 +0300

    game1

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import _ from 'lodash';
 import "./index.scss";
+
+const changeColorBnt = document.querySelector('.projects__btn');
+const body = document.querySelector('.page');
+
+changeColorBnt.addEventListener('click', function() {
+    
+    body.style.backgroudnd = 'rgb(' + Math.round(Math.random() + Math.random() * Math.random()) + Math.round(Math.random()) + ',' +  Math.round(Math.random())  +Math.round(Math.random()) + ',' + Math.round(Math.random()) +  Math.round(Math.random()) + ');';
+    console.log(body.style.backgroudnd);
+});
